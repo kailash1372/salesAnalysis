277b07162b9469a6fe76a0c790adb377344126c6
Kailash <86278203+kailash1372@users.noreply.github.com>
2023-06-13 08:15:31 +0530

    added 2 more charts on the products page

--- products/products.js
+++ products/products.js
@@ -33,9 +33,9 @@ var date = new Date();
 date = date.toDateString();
 date = date.split(' ');
 
-jsMonth = {"Jan":1,"Feb":2,"Mar":3,"Apr":4,"May":5,"Jun":6,"Jul":7,"Aug":8,"Sep":9,"Oct":10,"Nov":11,"Dec":12}
-monthMap = {1:"January",2:"February",3:"March",4:"April",5:"May",6:"June",7:"July",8:"August",9:"September",10:"October",11:"November",12:"December"}
-jsmonthvalue = monthMap[jsMonth[date[1]]];
+const jsMonth = {"Jan":1,"Feb":2,"Mar":3,"Apr":4,"May":5,"Jun":6,"Jul":7,"Aug":8,"Sep":9,"Oct":10,"Nov":11,"Dec":12}
+const monthMap = {1:"January",2:"February",3:"March",4:"April",5:"May",6:"June",7:"July",8:"August",9:"September",10:"October",11:"November",12:"December"}
+var jsmonthvalue = monthMap[jsMonth[date[1]]];
 
 var productunitspermonth = [['productName', 'monthlySales']];
 var index = 0;
@@ -50,13 +50,13 @@ ref.collection("allproducts").get().then((querySnapshot) => {
             index = index + 1;
 
             if (index === querySnapshot.size) {
-                drawMonthlySales();
+                drawProductSales();
             }
         });
     });
 });
 
-function drawMonthlySales() {
+function drawProductSales() {
     google.charts.load('current', { packages: ['corechart', 'bar'] });
     google.charts.setOnLoadCallback(() => {
         var data = google.visualization.arrayToDataTable(productunitspermonth);
@@ -66,8 +66,66 @@ function drawMonthlySales() {
 }
 
 
-// ref.collection("allproducts").get().then((querySnapshot)=>{
-//     querySnapshot.forEach((doc)=>{
+const MonthMap = new Map();
+var times = 0;
+var totalDocs = 0;
 
-//     });
-// });
+async function fetchData() {
+  const querySnapshot = await ref.collection("allproducts").get();
+  totalDocs = querySnapshot.size;
+//   console.log(totalDocs)
+
+  for (const doc of querySnapshot.docs) {
+    times++;
+    var pname = doc.id;
+    var sp = doc.data().sellingprice;
+    var m = 0;
+    const innerSnapshot = await ref.collection("allproducts").doc(pname).collection("data").get();
+
+    innerSnapshot.forEach((doc) => {
+      if (MonthMap.has(doc.id)) {
+        MonthMap.set(doc.id, MonthMap.get(doc.id) + doc.data().total);
+      } else {
+        MonthMap.set(doc.id, doc.data().total);
+      }
+      m++;
+    //   console.log(times + " " + m)
+      if (times === totalDocs && m === 12) {
+        monthlyTrend(MonthMap);
+      }
+    });
+  }
+}
+
+fetchData();
+
+function monthlyTrend(MonthMap) {
+  var arr = [['Month','Sales']];
+  var t = 0;
+  for (var i = 1; i <= 12; i++) {
+    var temp = [monthMap[i].substring(0,3), MonthMap.get(monthMap[i])];
+    arr.push(temp);
+    t+=MonthMap.get(monthMap[i]);
+    if(monthMap[i]==jsmonthvalue)drawPieChart(MonthMap.get(monthMap[i]),t)
+  }
+
+  google.charts.load('current', { packages: ['corechart', 'line'] });
+  google.charts.setOnLoadCallback(() => {
+      var data = google.visualization.arrayToDataTable(arr);
+      var chart = new google.visualization.LineChart(document.getElementById('monthlytrend'));
+      chart.draw(data);
+  });
+  
+}
+
+
+function drawPieChart(curVal,totVal){
+    var a = [['Month','sales'],['This Month',curVal],['All Months',totVal-curVal]];
+
+    google.charts.load('current', { packages: ['corechart'] });
+    google.charts.setOnLoadCallback(() => {
+        var data = google.visualization.arrayToDataTable(a);
+        var chart = new google.visualization.PieChart(document.getElementById('pieChart'));
+        chart.draw(data);
+    });
+}
